3b4042a8861d2841b94e5cd772d7d9349f507527
smajkrzak <s.majkrzak@gmail.com>
2016-02-26 21:27:33 +0100

    Some fonts

--- TheGame/Assets/Scripts/GameController.cs
+++ TheGame/Assets/Scripts/GameController.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private bool wave;
+    private int income = 500;
+
+    void Start () {
+        wave = false;
+    }
+
+    void StartWave () {
+        wave = true;
+        btn.setActive (false);
+    }
+
+
 }
