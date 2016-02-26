73d7c5d3e62472b89770a07f9c91f5b6536cce1f
Adam Dyngosz <Adam.Dyngosz@nokia.com>
2016-02-26 21:36:53 +0100

    moving to castle, checking distance

---
+++ TheGame/Assets/enemyScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyScript : MonoBehaviour {
+
+    public GameObject CASTLE;
+    public float range;
+    public float dmg;
+    public float attackRate; // seconds between attacks
+    public float moveSpeed; // units per second
+
+    private float lastAttackTime;
+
+	// Use this for initialization
+	void Start () {
+        lastAttackTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nextMove();
+	}
+
+    void nextMove()//move or attack
+    {
+        Vector3 path = pathToCastle();
+        float distance = path.x + path.z;//pseudo distance
+
+        if (distance <= range)
+        {
+            attack();
+            return;
+        }
+
+        float maxDelta = Time.deltaTime * moveSpeed;
+
+        if (distance > maxDelta)
+        {
+            Vector3 shortPath = new Vector3();
+            shortPath.x = path.x * maxDelta / (distance - path.z);
+            shortPath.z = path.z * maxDelta / (distance - path.x);
+            path = shortPath;
+        }
+        move(path);
+    }
+
+    void move(Vector3 step)
+    {
+        transform.position = transform.position + step;
+    }
+
+    Vector3 pathToCastle()
+    {
+        float xDiff = transform.position.x - CASTLE.transform.position.x;
+        float zDiff = transform.position.y - CASTLE.transform.position.y;
+
+        return new Vector3(xDiff, 0, zDiff);
+    }
+
+    void attack()
+    {
+        if(lastAttackTime < Time.time - attackRate)
+        {
+            //CASTLE.takeDmg(dmg);
+            lastAttackTime += attackRate;
+        }
+    }
+}
