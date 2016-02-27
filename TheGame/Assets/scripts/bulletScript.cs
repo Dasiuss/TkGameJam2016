9644815c8f3769a4ac4e1a7bea312d90db1123bb
Adam Dyngosz <Adam.Dyngosz@nokia.com>
2016-02-27 11:22:31 +0100

    bullets fixed

--- TheGame/Assets/scripts/bulletScript.cs
+++ TheGame/Assets/scripts/bulletScript.cs
@@ -1,49 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletScript : MonoBehaviour
-{
-    public float speed = 15;
+public class BulletScript : MonoBehaviour {
+    public float speed = 15.0f;
+    public Transform target;
+    public GameObject enemy;
+    public int dmg;
 
-    private Transform target;
-    private GameObject enemy;
-    private bool targetSetted = false;
-    private float dmg;
+	void Start () {
+	
+	}
+	
+	void Update () {
+        Vector3 dir = target.position = this.transform.localPosition;
+        float distThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distThisFrame) {
+            DoBulletHit ();
+        } else {
+            transform.Translate (dir.normalized * distThisFrame, Space.World);
+        }
+	}
 
-    void Start()
-    {
+    void DoBulletHit () {
+        Destroy (gameObject);
     }
 
-    void Update()
-    {
-
-        if (!targetSetted) return;
-
-        if (target == null)
-        { 
-            Destroy(gameObject);
-            return;
-        }
-
-        float maxDelta = Time.deltaTime * speed;
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, maxDelta);
-        float distance = Vector3.Distance(transform.position, target.transform.position);
-        if (distance < 0.2)
-        {
-            target.SendMessage("takeDmg", dmg);
-            Destroy(gameObject);
-        }
-    }
-
-    public void SetEnemy(object t)
-    {
+    public void SetEnemy (object t) {
         this.enemy = (GameObject)t;
         this.target = enemy.transform;
-        targetSetted = true;
     }
 
-    public void SetDmg(object dmg)
-    {
-        this.dmg = (float)dmg;
+    public void SetDMG (object dmg) {
+        this.dmg = (int)dmg;
     }
 }
